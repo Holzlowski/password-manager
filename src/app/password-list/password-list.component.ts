72e209decdd4846536e147a160bec405df695bba
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2025-06-25 23:30:19 +0200

    Add password management for individual sites
    Implemented CRUD operations for passwords associated with each site. Updated PasswordListComponent to display, add, edit, and delete passwords, and to receive site details via query parameters. Modified PasswordManagerService to support password operations within site subcollections. Updated templates to bind data dynamically and enable password management per site.

--- src/app/password-list/password-list.component.ts
+++ src/app/password-list/password-list.component.ts
@@ -1,11 +1,94 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { PasswordManagerService } from '../password-manager.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-password-list',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './password-list.component.html',
   styleUrl: './password-list.component.css'
 })
 export class PasswordListComponent {
 
+  siteId !: string;
+  siteName!: string;
+  siteURL!: string;
+  siteImgURL!: string;
+
+  passwordList!: Observable<Array<any>>;
+
+  email: string = '';
+  username: string = '';
+  password: string = '';
+  passwordId: string = '';
+
+  formState: string = "Add new";
+
+  constructor(private route: ActivatedRoute, private passwordService: PasswordManagerService) {
+
+    this.route.queryParams.subscribe((val: any) => {
+      this.siteId = val.id;
+      this.siteName = val.siteName;
+      this.siteURL = val.siteURL;
+      this.siteImgURL = val.siteImgURL;
+    })
+    this.loadPasswords();
+  }
+
+  resetForm() {
+    this.formState = "Add new";
+    this.email = '';
+    this.username = '';
+    this.password = '';
+    this.passwordId = '';
+  }
+
+  onSubmit(values: object) {
+    //console.log("Form submitted with values: ", values);
+    if (this.formState == "Add new") {
+      this.passwordService.addPassword(values, this.siteId)
+        .then(() => {
+          console.log("Password added successfully!");
+          this.resetForm();
+        })
+        .catch((error) => {
+          console.error("Error adding password: ", error);
+        })
+    } else if (this.formState == "Edit") {
+      this.passwordService.updatePassword(this.siteId, this.passwordId, values)
+        .then(() => {
+          console.log("Data updated successfully!");
+          this.resetForm();
+        })
+        .catch((error) => {
+          console.error("Error updating data: ", error);
+        })
+    }
+  }
+
+  loadPasswords() {
+    this.passwordList = this.passwordService.loadPasswords(this.siteId);
+  }
+
+  editPassword(email: string, username: string, password: string, id: string) {
+    this.formState = "Edit";
+    this.email = email;
+    this.username = username;
+    this.password = password;
+    this.passwordId = id;
+  }
+
+  deletePassword(passwordId: string) {
+    this.passwordService.deletePassword(this.siteId, passwordId)
+      .then(() => {
+        console.log("Password deleted successfully!");
+        this.resetForm();
+      })
+      .catch((error) => {
+        console.error("Error deleting password: ", error);
+      });
+  }
 }
--- src/app/password-manager.service.ts
+++ src/app/password-manager.service.ts
@@ -27,4 +27,26 @@ export class PasswordManagerService {
     const docInstance = doc(this.firestore, `sites/${id}`);
     return deleteDoc(docInstance);
   }
+
+  // password queries
+
+  addPassword(data: object, siteId: string) {
+    const dbInstance = collection(this.firestore, `sites/${siteId}/passwords`);
+    return addDoc(dbInstance, data);
+  }
+
+  loadPasswords(siteId: string) {
+    const dbInstance = collection(this.firestore, `sites/${siteId}/passwords`);
+    return collectionData(dbInstance, { idField: 'id' });
+  }
+
+  updatePassword(siteId: string, passwordId: string, data: object) {
+    const docInstance = doc(this.firestore, `sites/${siteId}/passwords/${passwordId}`);
+    return updateDoc(docInstance, data);
+  }
+
+  deletePassword(siteId: string, passwordId: string) {
+    const docInstance = doc(this.firestore, `sites/${siteId}/passwords/${passwordId}`);
+    return deleteDoc(docInstance);
+  }
 }
