d55f226389b8cf951b3523af6c1768c4f170e4d4
Holzlowski <43970216+Holzlowski@users.noreply.github.com>
2025-06-25 18:22:32 +0200

    feat: add password-list
    - Routing

--- src/app/site-list/site-list.component.ts
+++ src/app/site-list/site-list.component.ts
@@ -3,10 +3,11 @@ import { FormsModule } from '@angular/forms';
 import { PasswordManagerService } from '../password-manager.service';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-site-list',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './site-list.component.html',
   styleUrl: './site-list.component.css'
 })
